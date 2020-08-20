7dfa7f86eda8f14012997718f91556df705daaf9
Muteman30 <jameshyde30@gmail.com>
2020-08-20 08:47:14 +0100

    UpsdatedCustomerModel to include new fields Address,Postcode,telephone

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -2,12 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace NetwrixTest.Models
 {
     public class Customer
     {
         public int Id { get; set; }
+        [Required]
+        [MaxLength(100)]
         public string Name { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string Address1 { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string Address2 { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string Postcode { get; set; }
+        [Required]
+        [MaxLength(15)]
+        public string Telephone { get; set; }
     }
 }
