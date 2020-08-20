82e2da46c796b440656e609bdcd25438a433b745
Muteman30 <jameshyde30@gmail.com>
2020-08-20 10:47:49 +0100

    Added View to seee all customers

--- Controllers/CustomerController.cs
+++ Controllers/CustomerController.cs
@@ -3,15 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using NetwrixTest.Models;
+using NetwrixTest.ViewModels;
 
 namespace NetwrixTest.Controllers
 {
     public class CustomerController : Controller
     {
+        private ApplicationDbContext _context;
+        public CustomerController()
+        {
+            _context = new ApplicationDbContext();
+        }
         // GET: Customer
         public ActionResult Index()
         {
-            return View();
+            var vm = new AllCustomers
+            {
+                Customers = _context.Customers.ToList()
+            };
+            return View(vm);
         }
     }
 }
